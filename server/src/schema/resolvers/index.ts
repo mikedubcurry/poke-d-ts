adbdb6a574fd9ac30d48a9de818ab84b95c7e081
michaelcurry95 <fender18295@gmail.com>
2020-02-18 18:32:12 -0500

    async await resolver works; add redis before you over request the api

--- server/src/schema/resolvers/index.ts
+++ server/src/schema/resolvers/index.ts
@@ -3,16 +3,19 @@ import fetch from "node-fetch"
 
 const resolvers: IResolvers = {
 	Query: {
-		pokemonsAll: (parent, args, context, info) => {
-			return fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
-				.then(res => {
-					// console.log(res.json())
-					return res.json()
-				})
-				.then(({ results }) => {
-					console.log(results)
-					return results
-				})
+		pokemonsAll: async (parent, args, context, info) => {
+			let pokemons = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+			let { results } = await pokemons.json()
+			return results.map(async res => {
+				let pokeman = await fetch(res.url)
+				let pokemanJson = await pokeman.json()
+				return {
+					name: pokemanJson.name,
+					id: pokemanJson.id,
+					url: pokemanJson.url,
+					sprite: pokemanJson.sprites.front_default
+				}
+			})
 		},
 		hello: () => "Hello GraphQL"
 	}
