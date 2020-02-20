6909d4059e4e8699fd81cfba447943965b7b09d4
michaelcurry95 <fender18295@gmail.com>
2020-02-19 19:07:37 -0500

    dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,20 +2,21 @@ import express, { Application, Request, Response } from "express"
 import { ApolloServer } from "apollo-server-express"
 import cors from "cors"
 import compression from "compression"
+import { config } from "dotenv"
 
 import schema from "./schema"
 
+// load .env variables
+config()
+
+
 const app: Application = express()
 const server = new ApolloServer({ schema })
-
-app.get("/", (_req: Request, res: Response) => {
-	res.send("hi")
-})
 
 app.use("*", cors())
 app.use(compression())
 server.applyMiddleware({ app, path: "/graphql" })
 
 app.listen(3000, () => {
-	console.log("listening on port 3000")
+	console.log("listening on http://localhost:3000/graphql")
 })
