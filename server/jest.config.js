c506396c58c6d8098c7841d71ef60d0569346b39
michaelcurry95 <fender18295@gmail.com>
2020-02-21 20:35:20 -0500

    tests are on

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,5 +2,12 @@ module.exports = {
 	transform: { "^.+\\.ts?$": "ts-jest" },
 	testEnvironment: "node",
 	testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
-	moduleFileExtensions: ["ts", "js", "json", "node"]
+	moduleFileExtensions: ["ts", "js", "json", "node"],
+	globals: {
+    'ts-jest': {
+      diagnostics: {
+        warnOnly: true
+      }
+    }
+  }
 }
