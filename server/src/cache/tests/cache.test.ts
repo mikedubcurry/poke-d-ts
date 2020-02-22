8c76185a3bc25e4febf534a8bfffdf52d8712c52
michaelcurry95 <fender18295@gmail.com>
2020-02-21 23:24:08 -0500

    cache tests

---
+++ server/src/cache/tests/cache.test.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from "apollo-server-express"
+import { createTestClient } from "apollo-server-testing"
+
+import cache, { checkAll, checkBy, getPokemon } from "../../cache"
+import schema from "../../schema"
+
+const MAGIKARP_QUERY = `
+  query {
+    pokemon(id: 129) {
+      name
+      id
+      url
+      sprite
+    }
+  }
+`
+
+describe("Redis cache", () => {
+  beforeAll(() => {
+    cache.del('pokemons')
+    cache.del('129')
+  })
+	it("should populate redis if no entries found for pokemon", async () => {
+		const server = new ApolloServer({ schema })
+		const { query } = createTestClient(server)
+
+    const res = await query({ query: MAGIKARP_QUERY })
+    
+    const isInCache = await getPokemon('129')
+    expect(isInCache).not.toBe(null)
+  })
+})
