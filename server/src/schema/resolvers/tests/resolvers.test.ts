64cfe776bb663378233535954a878c82fd058406
michaelcurry95 <fender18295@gmail.com>
2020-02-21 20:37:06 -0500

    hello and pokemonByID tests

--- server/src/schema/resolvers/tests/resolvers.test.ts
+++ server/src/schema/resolvers/tests/resolvers.test.ts
@@ -1,6 +1,40 @@
-describe('test should pass', () => {
-  it('should run this test', () => {
-    let result = 'OK'
-    expect(result).toBe('OK')
-  })
+import { ApolloServer } from "apollo-server-express"
+import { createTestClient } from "apollo-server-testing"
+
+import schema from "../../../schema"
+
+// const server = new ApolloServer({ schema })
+
+// const { query, mutate } = createTestClient(server)
+const HELLO_QUERY = `
+  query {
+    hello
+  }
+`
+
+const MAGIKARP_QUERY = `
+  query {
+    pokemon(id: 129) {
+      name
+    }
+  }
+`
+
+describe("GraphQL queries", () => {
+	it('should return "Hello GraphQL"', async () => {
+		const server = new ApolloServer({ schema })
+
+		const { query } = createTestClient(server)
+
+		const res = await query({ query: HELLO_QUERY })
+		expect(res.data).toHaveProperty('hello', 'Hello GraphQL')
+	})
+
+	it("should return Magikarp", async () => {
+		const server = new ApolloServer({ schema })
+
+		const { query } = createTestClient(server)
+
+		const res = await query({ query: MAGIKARP_QUERY })
+	})
 })
